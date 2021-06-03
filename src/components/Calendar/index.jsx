dc4981c128231d597d7899bbf7d0c138b6ab83eb
EvgenStr <juve2910@gmail.com>
2021-06-03 21:29:12 +0300

    create week component

--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react';
+import style from './Calendar.module.sass';
+import { setDate, endOfMonth, eachWeekOfInterval } from 'date-fns';
+import Week from './Week';
+import CalendarHeader from './CalendarHeader';
 
-function Calendar() {
+export default function Calendar () {
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const weeksInMonthArray = eachWeekOfInterval({
+    start: setDate(currentDate, 1),
+    end: endOfMonth(currentDate),
+  });
+
+  console.dir(weeksInMonthArray);
+
+  const weeks = weeksInMonthArray.map(weekStartDate => (
+    <Week startDate={weekStartDate} key={weekStartDate.toLocaleDateString()} />
+  ));
   return (
-
-  )
+    <div className={style.container}>
+      <CalendarHeader />
+      {weeks}
+    </div>
+  );
 }
-export default Calendar;
