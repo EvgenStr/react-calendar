040c975eec22f879e7b54c3328681089809ddd5c
EvgenStr <juve2910@gmail.com>
2021-06-04 01:34:10 +0300

    add styles for CurrentDay

--- src/components/Calendar/CurrentDay/index.jsx
+++ src/components/Calendar/CurrentDay/index.jsx
@@ -4,9 +4,12 @@ import style from './CurrentDay.module.sass'
 
 function CurrentDay(props) {
   const { currentDate } = props;
+  const dayName = format(currentDate, 'EEEE');
+  const dayNumder = format(currentDate, 'd')
   return (
     <div className={style.container}>
-      {format(currentDate,'MM/dd/yyyy')}
+      <p className={style.dayName}>{dayName}</p>
+      <p className={style.dayNumber}>{dayNumder}</p>
     </div>
   )
 }
