24792a79439a4d58e17a41cac1cde6aa04e90f72
EvgenStr <juve2910@gmail.com>
2021-06-05 21:15:58 +0300

    create Header comp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import './App.css';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
-import Calendar from './components/Calendar';
+import Pages from './components/Pages'
 
 function App() {
   return (
     <BrowserRouter>
-    <Header />
-    <Switch>
-      <Route exact path='/'>
-        <Home />
-      </Route>
-      <Route path='/contacts'>
-        <Contacts />
-      </Route>
-      <Route path='/about' component={About} />
-      <Route path='/counter' component={CounterPage} />
-
-      <Route path='*' component={NotFound} />
-    </Switch>
-  </BrowserRouter>
+      <Pages />
+    </BrowserRouter>
   );
 }
 
--- src/components/Pages/index.jsx
+++ src/components/Pages/index.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 function Pages() {
   return (
     <div>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/contacts'>
+          <Contacts />
+        </Route>
+        <Route path='/about' component={About} />
+        <Route path='/counter' component={CounterPage} />
 
+        <Route path='*' component={NotFound} />
+      </Switch>
     </div>
   )
 }
