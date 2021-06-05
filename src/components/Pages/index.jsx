bf96bf61f100a5fa81dcb0a39d2fcb83aa6ed7af
EvgenStr <juve2910@gmail.com>
2021-06-05 21:18:15 +0300

    moved calendar to pages

--- src/components/Pages/index.jsx
+++ src/components/Pages/index.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Header from './Header';
 
 function Pages() {
   return (
-    <div>
-      <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route path='/contacts'>
-          <Contacts />
-        </Route>
-        <Route path='/about' component={About} />
-        <Route path='/counter' component={CounterPage} />
 
-        <Route path='*' component={NotFound} />
-      </Switch>
-    </div>
+    <Switch>
+      <Header />
+      <Route exact path='/'>
+        {/* <Home /> */}
+      </Route>
+      <Route path='/contacts'>
+        {/* <Contacts /> */}
+      </Route>
+      {/* <Route path='/about' component={About} />
+      <Route path='/counter' component={CounterPage} />
+      <Route path='*' component={NotFound} /> */}
+    </Switch>
+
   )
 }
 export default Pages;
