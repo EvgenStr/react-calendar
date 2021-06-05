70587974f61a44ef8f74d7d21cf5b91b9b007c95
EvgenStr <juve2910@gmail.com>
2021-06-05 21:13:30 +0300

    create Pages comp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import Calendar from './components/Calendar';
 
 function App() {
   return (
-    <Calendar />
+    <BrowserRouter>
+    <Header />
+    <Switch>
+      <Route exact path='/'>
+        <Home />
+      </Route>
+      <Route path='/contacts'>
+        <Contacts />
+      </Route>
+      <Route path='/about' component={About} />
+      <Route path='/counter' component={CounterPage} />
+
+      <Route path='*' component={NotFound} />
+    </Switch>
+  </BrowserRouter>
   );
 }
 
