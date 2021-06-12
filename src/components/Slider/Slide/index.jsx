f6241b849d2fdf8b01a4ef91428011d2fb27397f
EvgenStr <juve2910@gmail.com>
2021-06-13 02:45:12 +0300

    add target.remove() from errorHandler

--- src/components/Slider/Slide/index.jsx
+++ src/components/Slider/Slide/index.jsx
@@ -5,17 +5,18 @@ import style from './Slide.module.sass';
 function Slide(props) {
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
-  const errorHandler = () => setIsError(true);
-  const loadHandler = () => {
-    setIsLoading(false)
-    console.log('Slide')
+  const errorHandler = ({ target }) => {
+    target.remove();
+    setIsError(true)
   };
+  const loadHandler = () => setIsLoading(false);
   useEffect(() => { setIsLoading(true) }, [props])
   return (
     <div className={style.wrapper}>
       <img className={cx(style.currentSlide, { [style.active]: !isLoading })}
         onError={errorHandler} onLoad={loadHandler}
-        {...props} alt="space" />
+        {...props} alt="space"
+      />
       <div className={cx(style.error, { [style.errorActive]: isError })}>load error</div>
     </div>
   )
