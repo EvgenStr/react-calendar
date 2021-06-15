63d244bd3bd78e6771747f1a29c9c36bf3946624
EvgenStr <juve2910@gmail.com>
2021-06-15 12:00:49 +0300

    added opacity animations for Slide

--- src/components/Slider/Slide/index.jsx
+++ src/components/Slider/Slide/index.jsx
@@ -1,49 +1,61 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import SliderControls from '../SliderControls';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import style from './Slide.module.sass';
 
 function Slide({ prevSlideHandler, nextSlideHandler, src, prevSrc, nextSrc }) {
-  const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const currentImg = useRef(null)
+  const nextImg = useRef(null)
+  const prevImg = useRef(null)
+  const wrapper = useRef(null)
 
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(true)
-    }, 1000)
-  }, [])
   const errorHandler = ({ target }) => {
+    wrapper.current.style.opacity = 1
     target.remove();
     setIsError(true)
   };
 
+  const loadHandler = () => {
+    wrapper.current.style.opacity = 1
+    handlerSetter()
+  }
+
   const handlerSetter = () => {
-    setIsLoading(false);
-    setTimeout(() => {
-      setIsLoading(true)
-    }, 200)
+    currentImg.current.style.opacity = 1
+    nextImg.current.style.opacity = 0
+    prevImg.current.style.opacity = 0
   }
 
   const nextHandler = () => {
-    handlerSetter()
-    nextSlideHandler()
+    currentImg.current.style.opacity = 0
+    setTimeout(() => {
+      nextSlideHandler()
+    }, 200)
+
   }
 
   const prevHandler = () => {
-    handlerSetter()
-    prevSlideHandler()
+    currentImg.current.style.opacity = 0
+    setTimeout(() => {
+      prevSlideHandler()
+    }, 200)
+
   }
+  useEffect(() => {
+    currentImg.current.style.opacity = 0
+  }, [])
 
   return (
-    <div className={style.wrapper}>
-      <img className={cx(style.currentSlide, { [style.active]: isLoading })}
-        onError={errorHandler} src={src} alt="space"
+    <div ref={wrapper} className={style.wrapper}>
+      <img ref={currentImg} className={style.currentSlide}
+        onError={errorHandler} onLoad={loadHandler} src={src} alt="space"
       />
-      <img className={style.otherSlide}
+      <img ref={prevImg} className={style.otherSlide}
         onError={errorHandler} src={prevSrc} alt="space"
       />
-      <img className={style.otherSlide}
+      <img ref={nextImg} className={style.otherSlide}
         onError={errorHandler} src={nextSrc} alt="space"
       />
       <div className={cx(style.error, { [style.errorActive]: isError })}>load error</div>
