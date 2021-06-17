85f81e60f0386f119b7a959d5909a76d48b7d448
EvgenStr <juve2910@gmail.com>
2021-06-17 22:35:25 +0300

    add styles for todo item

--- src/components/ToDoList/ListItem/index.jsx
+++ src/components/ToDoList/ListItem/index.jsx
@@ -5,11 +5,11 @@ import style from './ListItem.module.sass';
 function ListItem({ isDoneHandler, deleteItem, item }) {
   // console.log(item)
   return (<>
-    <li className={cx(style.ListItem, { [style.done]: item.isDone })}>
+    <li className={style.listItem}>
       <input onChange={() => isDoneHandler(item.id)} type="checkbox"></input>
-      <span>{item.name}</span>
+      <span className={cx(style.text, { [style.done]: item.isDone })}>{item.name}</span>
 
-      <button onClick={() => deleteItem(item.id)}>delete</button>
+      <button className={style.btnDel} onClick={() => deleteItem(item.id)}>delete</button>
     </li>
 
 
