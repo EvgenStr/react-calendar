af0ed348df938472947575684431f4d8627e2e82
EvgenStr <juve2910@gmail.com>
2021-06-17 22:20:09 +0300

    add styles for todolist

--- src/components/ToDoList/index.jsx
+++ src/components/ToDoList/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import List from './List';
-import AddItemInput from './AddItemInput'
-
+import AddItemInput from './AddItemInput';
+import style from './ToDoList.module.sass';
 
 function ToDoList() {
   const [toDo, setTodo] = useState([{ id: 1, name: 'buy milk', isDone: false }, { id: 2, name: 'test', isDone: true }, { id: 3, name: 'some text', isDone: false }])
@@ -18,7 +18,7 @@ function ToDoList() {
     })));
     // console.log(toDo, 'isdone', id)
   }
-  return (<section>
+  return (<section className={style.container}>
     <AddItemInput addNewItem={addNewItem} />
     <List data={toDo} deleteItem={deleteItem} isDoneHandler={isDoneHandler}></List>
   </section>
