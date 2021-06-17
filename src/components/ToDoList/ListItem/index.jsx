89407df972a627e27cd623abb3ef53f2e1839fbd
EvgenStr <juve2910@gmail.com>
2021-06-18 00:25:12 +0300

    add material checkbox and button

--- src/components/ToDoList/ListItem/index.jsx
+++ src/components/ToDoList/ListItem/index.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import cx from 'classnames';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
+import DoneIcon from '@material-ui/icons/Done';
+import Checkbox from '@material-ui/core/Checkbox';
+
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 import style from './ListItem.module.sass';
 
-function ListItem({ isDoneHandler, deleteItem, item }) {
+function ListItem({ isDoneHandler, deleteItem, item: { name, id, isDone } }) {
   // console.log(item)
   return (<>
     <li className={style.listItem}>
-      <input onChange={() => isDoneHandler(item.id)} type="checkbox"></input>
-      <span className={cx(style.text, { [style.done]: item.isDone })}>{item.name}</span>
-
-      <button className={style.btnDel} onClick={() => deleteItem(item.id)}>delete</button>
+      <FormControlLabel
+        control={<Checkbox onChange={() => isDoneHandler(id)} icon={<CheckBoxOutlineBlankIcon />}
+          checkedIcon={<DoneIcon />}
+          checked={isDone}
+          name="checkedH" />}
+      />
+      <span className={cx(style.text, { [style.done]: isDone })}>{name}</span>
+      <IconButton onClick={() => deleteItem(id)} color="secondary" aria-label="delete">
+        <DeleteIcon />
+      </IconButton>
     </li>
 
 
