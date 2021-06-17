d26587b85e1cb923f1975ad8e2bc13183c76571c
EvgenStr <juve2910@gmail.com>
2021-06-17 22:35:07 +0300

    add styles for todo list

--- src/components/ToDoList/List/index.jsx
+++ src/components/ToDoList/List/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ListItem from '../ListItem';
+import style from './List.module.sass';
 
 function List({ data, deleteItem, isDoneHandler }) {
  
   const toDoElems = data.map((toDo) => <ListItem key={toDo.id} deleteItem={deleteItem} item={toDo} isDoneHandler={isDoneHandler} />)
   return (
-    <ul>
+    <ul className={style.list}>
       {toDoElems}
     </ul>
   )
