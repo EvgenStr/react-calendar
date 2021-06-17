bb5d11497c84707d9edb2ef6446a03a403e6f90c
EvgenStr <juve2910@gmail.com>
2021-06-17 22:55:15 +0300

    refactor for use custom hook

--- src/components/ToDoList/index.jsx
+++ src/components/ToDoList/index.jsx
@@ -1,26 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import List from './List';
 import AddItemInput from './AddItemInput';
+import { useToDoList } from 'hooks'
 import style from './ToDoList.module.sass';
 
 function ToDoList() {
-  const [toDo, setTodo] = useState([{ id: 1, name: 'buy milk', isDone: false }, { id: 2, name: 'test', isDone: true }, { id: 3, name: 'some text', isDone: false }])
-  const addNewItem = (item) => {
-    setTodo([...toDo, item])
-  }
-  const deleteItem = (id) => {
-    setTodo(toDo.filter(item => item.id !== id))
-  }
-  const isDoneHandler = (id) => {
-    setTodo(toDo.map((item) => ({
-      ...item,
-      isDone: item.id === id ? !item.isDone : item.isDone,
-    })));
-    // console.log(toDo, 'isdone', id)
-  }
+  const [toDo, addNewItem, deleteItem, isDoneHandler] = useToDoList();
+ 
   return (<section className={style.container}>
-    <AddItemInput addNewItem={addNewItem} />
     <List data={toDo} deleteItem={deleteItem} isDoneHandler={isDoneHandler}></List>
+    <AddItemInput addNewItem={addNewItem} />
   </section>
   )
 }
