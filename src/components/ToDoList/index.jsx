0f9543da7ef659fa9b42bec443467df89fd7a63f
EvgenStr <juve2910@gmail.com>
2021-06-17 23:12:24 +0300

    add toggler for add new item

--- src/components/ToDoList/index.jsx
+++ src/components/ToDoList/index.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import List from './List';
 import AddItemInput from './AddItemInput';
-import { useToDoList } from 'hooks'
+import { useToDoList } from 'hooks';
 import style from './ToDoList.module.sass';
 
 function ToDoList() {
-  const [toDo, addNewItem, deleteItem, isDoneHandler] = useToDoList();
- 
-  return (<section className={style.container}>
-    <List data={toDo} deleteItem={deleteItem} isDoneHandler={isDoneHandler}></List>
-    <AddItemInput addNewItem={addNewItem} />
-  </section>
-  )
+  const [
+    toDo,
+    isAddActive,
+    setIsAddActive,
+    addNewItem,
+    deleteItem,
+    isDoneHandler,
+  ] = useToDoList();
+
+  return (
+    <section className={style.container}>
+      <List
+        data={toDo}
+        deleteItem={deleteItem}
+        isDoneHandler={isDoneHandler}
+      ></List>
+      {isAddActive && (
+        <button
+          className={style.addToggler}
+          onClick={() => setIsAddActive(!isAddActive)}
+        >
+          {'+ New task'}
+        </button>
+      )}
+      {!isAddActive && <AddItemInput addNewItem={addNewItem} />}
+    </section>
+  );
 }
 export default ToDoList;
