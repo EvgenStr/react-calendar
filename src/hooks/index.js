e9636559ad687c2bc8b7bc6e75ebf37bbac668aa
EvgenStr <juve2910@gmail.com>
2021-06-17 22:54:58 +0300

    create custom hook useToDoList

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -14,8 +14,27 @@ export function useData(loadData) {
       })
       .catch(err => setError(err))
       .finally(() => setIsFetching(false));
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return { data, error, isFetching };
+}
+
+export function useToDoList() {
+  const [toDo, setTodo] = useState([
+    { id: 1, name: 'buy milk', isDone: false },
+    { id: 2, name: 'test', isDone: true },
+    { id: 3, name: 'some text', isDone: false }])
+    const addNewItem = (item) => {
+      setTodo([...toDo, item])
+    }
+    const deleteItem = (id) => {
+      setTodo(toDo.filter(item => item.id !== id))
+    }
+    const isDoneHandler = (id) => {
+      setTodo(toDo.map((item) => ({
+        ...item,
+        isDone: item.id === id ? !item.isDone : item.isDone,
+      })));}
+    return ([toDo, addNewItem, deleteItem, isDoneHandler])
 }
