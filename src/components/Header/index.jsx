35098bfb59c610cddbd5d028a98bb43dbefe4294
EvgenStr <juve2910@gmail.com>
2021-06-19 19:26:17 +0300

    fix header styles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,22 +2,28 @@ import React from 'react';
 import NavItem from './NavItem';
 import style from './Header.module.sass';
 
-function Header() {
+function Header () {
   return (
     <header className={style.header}>
       <nav className={style.navMenu}>
-        <ul className={style.navList}>
-          <NavItem to='/'>Home</NavItem>
-          <NavItem to='/calendar'>Calendar</NavItem>
-          <NavItem to='/counter'>Counter</NavItem>
-          <NavItem to='/user-list'>User-List</NavItem>
-          <NavItem to='/slider'>Slider</NavItem>
-          <NavItem to='/todo'>ToDo</NavItem>
-          <NavItem to='/sign-up'>Sign Up</NavItem>
-          <NavItem to='/login'>Login</NavItem>
-        </ul>
+        <div className={style.leftSide}>
+          <ul className={style.navList}>
+            <NavItem to='/'>Home</NavItem>
+            <NavItem to='/calendar'>Calendar</NavItem>
+            <NavItem to='/counter'>Counter</NavItem>
+            <NavItem to='/user-list'>User-List</NavItem>
+            <NavItem to='/slider'>Slider</NavItem>
+            <NavItem to='/todo'>ToDo</NavItem>
+          </ul>
+        </div>
+        <div className={style.rightSide}>
+          <ul className={style.navList}>
+            <NavItem to='/sign-up'>Sign Up</NavItem>
+            <NavItem to='/login'>Login</NavItem>{' '}
+          </ul>
+        </div>
       </nav>
     </header>
-  )
+  );
 }
 export default Header;
---
+++ src/components/forms/SignUpForm/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import style from 'SignUpForm.module.sass'
+
+function SignUpForm () {
+  return <section className={style.container}>
+    
+  </section>;
+}
+export default SignUpForm;
