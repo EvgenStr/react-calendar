3cb7a52773a0eeeae22f27b0b770b437cdc10b0e
EvgenStr <juve2910@gmail.com>
2021-06-20 02:36:58 +0300

    add styles to FormGroup

--- src/components/forms/SignUpForm/FormGroup/index.jsx
+++ src/components/forms/SignUpForm/FormGroup/index.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import FormInput from '../../FormInput';
 import style from './FormGroup.module.sass';
 
-
 export default function FormGroup (props) {
-
-const {inpNames:{names, placeholders}, formikProps} =props
+  const {
+    inpNames: { names, placeholders },
+    formikProps,
+  } = props;
   return (
     <div className={style.group}>
       <FormInput formikProps={formikProps} name={names[0]}>
-      {placeholders[0]}
+        {placeholders[0]}
       </FormInput>
       <FormInput formikProps={formikProps} name={names[1]}>
-      {placeholders[1]}
+        {placeholders[1]}
       </FormInput>
     </div>
   );
