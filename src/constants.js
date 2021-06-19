a51e58df9b05d47d666dfadba14076e1f306c4e2
EvgenStr <juve2910@gmail.com>
2021-06-19 22:33:54 +0300

    add INPUT_NAMES to constants

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const INPUT_NAMES = {
+  firstGroup: {
+    names: ['firstName', 'lastName'],
+    placeholders: ['First Name', 'Last Name'],
+  },
+  secondGroup: {
+    names: ['displayName', 'email'],
+    placeholders: ['Display Name', 'Email Address'],
+  },
+  thirdGroup: {
+    names: ['password', 'passwordConfirmation'],
+    placeholders: ['Password', 'Password Confirmation'],
+  },
+};
