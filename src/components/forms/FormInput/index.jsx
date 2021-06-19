e97b3753c8cec974df9b0f1b754f7bf628b6ad02
EvgenStr <juve2910@gmail.com>
2021-06-19 22:35:51 +0300

    add formikprops to inputs component

--- src/components/forms/FormInput/index.jsx
+++ src/components/forms/FormInput/index.jsx
@@ -3,18 +3,24 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 import cx from 'classnames';
 import style from './FormInput.module.sass';
 
-function FormInput ({formikProps:{isValid, errors, dirty}}) {
+function FormInput ({
+  name,
+  children,
+  formikProps: { isValid, errors, dirty },
+  type = 'text',
+}) {
   return (
     <>
       <Field
-        className={cx(style.addForm, {
-          [style.inputError]:
-            !isValid && errors.firstName,
+        className={cx(style.input, {
+          [style.inputError]: !isValid && errors[name],
           [style.inputValid]: isValid && dirty,
         })}
-        name='firstName'
+        name={name}
+        type={type}
+        placeholder={children}
       />
-      <ErrorMessage className={style.error} name='firstName' component='span' />
+      <ErrorMessage className={style.error} name={name} component='span' />
     </>
   );
 }
